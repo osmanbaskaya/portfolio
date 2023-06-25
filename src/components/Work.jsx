cc972317aa4c9e464339b1417c90932776cb1094
Osman Baskaya <osmanbaskaya1@gmail.com>
2023-06-25 10:17:06 -0700

    adding hardhat info

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,4 @@
-import { Fingerprint } from '@mui/icons-material'
+import { Fingerprint } from "@mui/icons-material";
 import {
   Box,
   Chip,
@@ -8,8 +8,9 @@ import {
   Paper,
   Stack,
   Typography,
-} from '@mui/material'
-import HomeDivider from './HomeDivider'
+} from "@mui/material";
+import HomeDivider from "./HomeDivider";
+import { Fragment } from "react";
 
 function Section(props) {
   return (
@@ -20,23 +21,23 @@ function Section(props) {
           m: 0.5,
           marginBottom: 2,
           p: 1,
-          margin: 'auto',
+          margin: "auto",
           //   flexGrow: 1,
           backgroundColor: (theme) =>
-            theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+            theme.palette.mode === "dark" ? "#1A2027" : "#fff",
         }}
       >
         <Grid container justifyContent="space-between" alignItems="center">
           <Grid item md={6}>
-            <Box sx={{ display: 'flex', alignItems: 'center' }} width="100%">
+            <Box sx={{ display: "flex", alignItems: "center" }} width="100%">
               <Box
                 component="img"
                 sx={{
-                  height: '70px',
-                  justifyContent: 'flex-end',
+                  height: "70px",
+                  justifyContent: "flex-end",
                 }}
-                alt="twilio"
-                src="assets/img/twilio.png"
+                alt={props.altText}
+                src={props.logo}
               ></Box>
               <Typography variant="h3">{props.title}</Typography>
             </Box>
@@ -52,9 +53,9 @@ function Section(props) {
                       label={role}
                       color="success"
                       sx={{
-                        fontSize: 'medium',
-                        margin: '3px',
-                        // fontFamily: 'BIZ UDPMincho, serif',
+                        fontSize: "12px",
+                        margin: "3px",
+                        fontFamily: "Lora",
                       }}
                       key={index}
                       // variant="outlined"
@@ -89,9 +90,9 @@ function Section(props) {
                 aria-label="fingerprint"
                 // color="secondary"
                 sx={{
-                  '&:hover': {
-                    background: 'none',
-                    color: '#b09191',
+                  "&:hover": {
+                    background: "none",
+                    color: "#b09191",
                     // opacity: [0.9],
                   },
                 }}
@@ -102,7 +103,7 @@ function Section(props) {
                   component="a"
                   href={props.url}
                   variant="h6"
-                  sx={{ textDecoration: 'none' }}
+                  sx={{ textDecoration: "none" }}
                   color="green"
                   // fontFamily="BIZ UDPMincho, serif"
                 >
@@ -115,21 +116,43 @@ function Section(props) {
         </Grid>
       </Paper>
     </Box>
-  )
+  );
 }
 
 export default function Work() {
   return (
-    <Box>
-      <Section
-        title="Central AI"
-        short="Centralized Machine Learning"
-        url="https://www.twilio.com/speech-recognition"
-        description="I manage a team of software engineers and ML scientists responsible for helping other teams to apply Machine Learning to problems such as Speech Recognition, Spam/Phishing, Propensity to Buy, Send Time Optimization, and so on. "
-        roles={['Engineering Manager', 'Machine Learning Platform']}
-        summary="A platform that harnesses Machine Learning and enables the teams within the company such as Twilio Voice, Flex, Consumer Trust, and Segment build more intelligent products."
-      ></Section>
-      {/* <Divider
+    <Fragment>
+      <HomeDivider id="work" label="Imagination AI"></HomeDivider>
+      <Box>
+        <Section
+          title="Hard Hat"
+          short="Productivity app for tradespeople"
+          logo="assets/img/hardhat-logo.png"
+          altText="Twilio"
+          url="https://portfolio.baskaya.io/projects/hardhat/CodeLookup"
+          description="We have talked with dozens of mixture of electricians, plumbers, HVAC people. Currently, we are working on MVP."
+          roles={["GenAI", "Productivity", "Tradespeople"]}
+          summary="Hard Hat is a tradesperson productivity app to keep work moving. 
+          The demand for trade services (e.g. plumbing, HVAC) is at an all-time high, 
+          led by growth in residential development (up 15% YoY in 2022), but this demand can’t be fully realized 
+          in an industry hamstrung by productivity challenges such as skilled worker shortage and supply chain disruption. 
+          To realize this unmet demand tradespeople have to do more with less, a type of productivity problem that GenAI promises to help with."
+        ></Section>
+      </Box>
+
+      <HomeDivider id="work" label="TWILIO"></HomeDivider>
+      <Box>
+        <Section
+          title="Twilio AI"
+          logo="assets/img/twilio.png"
+          altText="Twilio"
+          short="Centralized Machine Learning"
+          url="https://www.twilio.com/speech-recognition"
+          description="I manage a team of software engineers and ML scientists responsible for helping other teams to apply Machine Learning to problems such as Speech Recognition, Spam/Phishing, Propensity to Buy, Send Time Optimization, and so on. "
+          roles={["Engineering Manager", "Machine Learning Platform"]}
+          summary="A platform that harnesses Machine Learning and enables the teams within the company such as Twilio Voice, Flex, Consumer Trust, and Segment build more intelligent products."
+        ></Section>
+        {/* <Divider
         sx={{
           borderWidth: '5',
           borderColor: 'black',
@@ -138,23 +161,26 @@ export default function Work() {
         }}
       ></Divider> */}
 
-      <Section
-        title="Autopilot"
-        short="Omnichannel AI assistant"
-        url="https://www.twilio.com/blog/introducing-twilio-autopilot-a-conversational-ai-platform-to-build-bots-that-work"
-        description="I lead the team and am the owner of the whole process of sunsetting Twilio Autopilot."
-        roles={[
-          'Product Management',
-          'Engineering Manager',
-          'Natural Language Understanding',
-          'Technical Leader',
-          'Chat bots',
-        ]}
-        summary="Autopilot is Twilio’s conversational AI platform for building
+        <Section
+          title="Autopilot"
+          short="Omnichannel AI assistant"
+          url="https://www.twilio.com/blog/introducing-twilio-autopilot-a-conversational-ai-platform-to-build-bots-that-work"
+          description="I lead the team and am the owner of the whole process of sunsetting Twilio Autopilot."
+          logo="assets/img/twilio.png"
+          altText="Twilio"
+          roles={[
+            "Product Management",
+            "Engineering Manager",
+            "Natural Language Understanding",
+            "Technical Leader",
+            "Chat bots",
+          ]}
+          summary="Autopilot is Twilio’s conversational AI platform for building
                 bots, IVRs, voice assistants, and more. Autopilot combines
                 Twilio’s Natural Language Understanding engine and Autopilot
                 Actions API."
-      ></Section>
-    </Box>
-  )
+        ></Section>
+      </Box>
+    </Fragment>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import { Divider, Typography } from '@mui/material'
-import Articles from '../components/Articles'
-import Hero from '../components/Hero'
-import HomeDivider from '../components/HomeDivider'
-import Work from '../components/Work'
+import { Divider, Typography } from "@mui/material";
+import Articles from "../components/Articles";
+import Hero from "../components/Hero";
+import HomeDivider from "../components/HomeDivider";
+import Work from "../components/Work";
 
 export default function Home() {
   return (
@@ -10,20 +10,21 @@ export default function Home() {
       <Hero />
       <Divider
         sx={{
-          borderWidth: '5',
-          borderColor: 'black',
-          marginTop: '2em',
-          marginBottom: '1em',
+          borderWidth: "5",
+          borderColor: "black",
+          marginTop: "2em",
+          marginBottom: "1em",
         }}
       ></Divider>
       <Typography variant="h5" fontStyle="italic">
         As an <strong>engineering leader and a Machine Learning expert</strong>,
         I'm driven by the limitless potential of technology to positively
         transform our world. With years of experience in the academy and
-        industry, being part of and leading{' '}
+        industry, being part of and leading{" "}
         <strong>cross-functional teams</strong>, I've developed a unique
         perspective on leveraging machine learning to solve complex problems and
-        create innovative solutions that deliver <strong>incremental value to customers</strong>.
+        create innovative solutions that deliver{" "}
+        <strong>incremental value to customers</strong>.
       </Typography>
       <Typography variant="h5" marginTop={2} fontStyle="italic">
         I'm passionate about developing and inspiring individuals to realize
@@ -35,16 +36,16 @@ export default function Home() {
       </Typography>
       <Typography variant="h5" marginTop={2} fontStyle="italic">
         I have a decade of experience in Natural Language Processing and Machine
-        learning which helped me publish my research in venues like{' '}
+        learning which helped me publish my research in venues like{" "}
         <strong>
-          EACL, the Journal of Artificial Intelligence Research, and SemEval{' '}
+          EACL, the Journal of Artificial Intelligence Research, and SemEval{" "}
         </strong>
         .
       </Typography>
-      <HomeDivider id="work" label="MY WORK @ TWILIO"></HomeDivider>
+
       <Work></Work>
       <HomeDivider id="research" label="RESEARCH"></HomeDivider>
       <Articles />
     </>
-  )
+  );
 }
