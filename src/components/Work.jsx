7e3e77263904659e4690a8e428ac7b5cc1270643
Osman Baskaya <osmanbaskaya1@gmail.com>
2023-06-25 12:02:50 -0700

    adding hardhat info

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -130,7 +130,7 @@ export default function Work() {
           logo="assets/img/hardhat-logo.png"
           altText="Twilio"
           url="https://portfolio.baskaya.io/projects/hardhat/CodeLookup"
-          description="We have talked with dozens of mixture of electricians, plumbers, HVAC people. Currently, we are working on MVP."
+          description="We have engaged in discussions with numerous electricians, plumbers, and HVAC professionals. Presently, we are diligently focusing on the development of our Minimum Viable Product."
           roles={["GenAI", "Productivity", "Tradespeople"]}
           summary="Hard Hat is a tradesperson productivity app to keep work moving. 
           The demand for trade services (e.g. plumbing, HVAC) is at an all-time high, 
